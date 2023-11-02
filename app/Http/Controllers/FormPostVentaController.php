bab961eade7f61eb224d184989ad3ec118f1302f
AnthonyDev <josealarconobando@gmail.com>
2023-11-02 00:56:00 -0500

    creacion de apis para postsale usando el controlador y modelo ya creados

--- app/Http/Controllers/FormPostVentaController.php
+++ app/Http/Controllers/FormPostVentaController.php
@@ -556,4 +556,78 @@ class FormPostVentaController extends Controller
     {
         //
     }
+
+    public function news($userid){
+        // Obtén todos los contrato desde la base de datos
+        $postsales = PostSale::select()
+        //->join('time_ranges', 'time_ranges.id', '=', 'post_sale.time_ranges_id ')
+        //->join('area', 'area.id_area', '=', 'clientearea.id_area')
+        //->join('cliente', 'contrato.id_cliente', '=', 'cliente.id_cliente')
+        //->join('tipocontrato', 'contrato.id_tipocontrato', '=', 'tipocontrato.id_tipocontrato')
+        //->join('estadocontrato', 'contrato.id_estadocontrato', '=', 'estadocontrato.id_estadocontrato')
+        ->where('motorized_id', $userid)
+        ->limit(4)
+        ->get();
+        if($postsales->count() > 0){
+            $success = true;
+            $message = 'Tareas listadas';
+        }else{
+            $success = false;
+            $message = 'No se encontraron tareas';
+            $postsales = '';
+        }
+        return compact('success', 'message', 'postsale');
+    }
+
+    public function olds($userid){
+        // Obtén todos los contrato desde la base de datos
+        $postsales = PostSale::select()
+        //->join('time_ranges', 'time_ranges.id', '=', 'post_sale.time_ranges_id ')
+        //->join('area', 'area.id_area', '=', 'clientearea.id_area')
+        //->join('cliente', 'contrato.id_cliente', '=', 'cliente.id_cliente')
+        //->join('tipocontrato', 'contrato.id_tipocontrato', '=', 'tipocontrato.id_tipocontrato')
+        //->join('estadocontrato', 'contrato.id_estadocontrato', '=', 'estadocontrato.id_estadocontrato')
+        ->where('motorized_id', $userid)
+        //->limit(4)
+        ->get();
+        if($postsales->count() > 0){
+            $success = true;
+            $message = 'Tareas listadas';
+        }else{
+            $success = false;
+            $message = 'No se encontraron tareas';
+            $postsales = '';
+        }
+        return compact('success', 'message', 'postsale');
+    }
+
+    public function showforapp($idpostsale){
+        $postsale=PostSale::where('id', $idpostsale)->first();
+        if($postsale->count() > 0){
+            $success = true;
+            $message = 'Tarea Encontrada';
+        }else{
+            $success = false;
+            $message = 'Error con la tarea';
+            $postsale = '';
+        }
+        return compact('success', 'message', 'postsale');
+    }
+
+    public function storeapp(Request $request){
+        // Accede a los datos JSON enviados en la solicitud
+        $data = $request->json()->all();
+        //dd($data);
+        // Procesa los datos y realiza la lógica para guardar la tarea en la base de datos
+        $postsale=PostSale::create($data);
+        // Devuelve una respuesta
+        if($postsale){
+            $success = true;
+            $message = 'Tarea Registrada Correctamente';
+        }else{
+            $success = false;
+            $message = 'Error con la tarea';
+        }
+        return compact('success', 'message');
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,22 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\FormPostVentaController;
+use App\Http\Controllers\TaskController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/login', [App\Http\Controllers\Api\AuthController::class, 'login']);
+Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:api')->group(function(){
-    Route::post('/logout', [App\Http\Controllers\Api\AuthController::class, 'logout']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
+
+Route::prefix('postventa')->middleware('auth:api')->group(function () {
+    // Rutas relacionadas con las task
+    Route::get('news/{userid}', [FormPostVentaController::class, 'news']); // Lista de tareas nuevas
+    Route::get('olds/{userid}', [FormPostVentaController::class, 'olds']); // Lista de tareas antiguas
+    Route::get('/{id}', [FormPostVentaController::class, 'showforapp']); // Ver una tarea
+    Route::post('/', [FormPostVentaController::class, 'storeapp']); // Registrar una tarea
+    Route::put('/{id}', [FormPostVentaController::class, 'update']); // Modificar una tarea (falta crear su api)
 });
